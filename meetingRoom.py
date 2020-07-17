d7a34b0f98f211f7b20998f41878b20aef4bb008
IlmunKoo <mun90505@naver.com>
2020-07-17 22:58:17 +0900

    백준 알고리즘 푼 것들

---
+++ meetingRoom.py
@@ -0,0 +1,17 @@
+import sys
+reserveList = [list(map(int, sys.stdin.readline().split())) for _ in range(int(input()))]
+reserveList.sort(key= lambda x: (x[1], x[0]))
+largest = [reserveList[0]]
+i = 1
+j = 0
+while True:
+    if largest[j][1] <= reserveList[i][0]:
+            largest.append(reserveList[i])
+            i += 1
+            j += 1
+    else:
+        i += 1
+        
+    if i > len(reserveList) - 1:
+        break
+print(len(largest))
