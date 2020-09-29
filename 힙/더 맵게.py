0e8fa78bcc1b631bace1f7d12b30226e4f6b7229
= <mun90505@naver.com>
2020-09-29 22:56:12 +0900

    힙 최종

--- 힙/더 맵게.py
+++ 힙/더 맵게.py
@@ -4,7 +4,6 @@ def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville) #리스트의 heap화
     while True:
-        
         a = heapq.heappop(scoville)  # heappop() 하는 순간 heap 내부에서 이진트리 재배치(새 최소값 발생)
         if a >= K:
             return answer
