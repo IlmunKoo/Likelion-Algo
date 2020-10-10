d474e4849e7fd930721a95097ad6bf00a4ceefc9
= <mun90505@naver.com>
2020-10-10 22:01:02 +0900

    1010

---
+++ 9663_test.py
@@ -0,0 +1,23 @@
+#내 윗줄에 나와 겹치는 라인에 퀸이 있는가?
+def adjacent(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == x - i:
+            return False
+    return True
+
+#한줄씩 재귀하며 DFS를 실행
+def dfs(x):
+    global result
+    
+    if x == N:
+        result += 1
+
+    else:
+        for i in range(N):
+            row[x] = i
+            if adjacent(x): #false 리턴하면 dfs 호출할 수 없으므로 dfs 종료
+                dfs(x + 1)
+
+
+# 어차피 윗 줄만 검사하면 되기 때문에
+# row[x] = i 윗줄을 먼저 확인해주고 인접을 확인한다.
